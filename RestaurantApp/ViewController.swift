0e80dcd6def4d4cf642bf094add167dca49de1cb
MohammedAfsal <afsalkp.mohammed@gmail.com>
2020-06-12 00:19:13 +0400

    Initia commit

---
+++ RestaurantApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantApp
+//
+//  Created by Afsal's Macbook Pro on 12/06/2020.
+//  Copyright © 2020 Afsal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
